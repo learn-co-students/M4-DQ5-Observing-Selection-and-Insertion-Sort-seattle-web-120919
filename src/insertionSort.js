8abd8a82d09bf17cea9c26c782ebee27a69bc849
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-05-02 14:07:08 -0400

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,11 @@
+// arr is an array of unsorted integers (i.e. [3, 5, 1])
+// i = start index
 function insertionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for (let i = 0; i < arr.length; i++) {
+    for(let currIdx = i; currIdx > 0 && arr[currIdx-1] > arr[currIdx]; currIdx--) {
+      let temp = arr[currIdx]
+      arr[currIdx] = arr[currIdx-1]
+      arr[currIdx-1] = temp
+    }
+  }
 }
